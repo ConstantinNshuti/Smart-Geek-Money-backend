8a06d5d6806eb975eca5fd513803145abc705dc6
Constantin <92950536+ConstantinNshuti@users.noreply.github.com>
2025-03-22 18:03:39 +0100

    Repositories erstellt

---
+++ src/main/java/com/geekinstitut/smartmoney/entity/Income.java
@@ -0,0 +1,11 @@
+package com.geekinstitut.smartmoney.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Income extends Transaction {
+}
